3a7cbe04a0fdb48b2058a1bcbb1ae9c7f962b611
Victor Chung <victochung998@yahoo.com>
2019-10-21 21:39:17 -0700

    clear cache

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ require("./routes/html-routes")(app);
 require("./routes/Authentication-routes")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force:false}).then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT);
   });
