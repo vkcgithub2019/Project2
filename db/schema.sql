744fbb3864ebd3041e39b0bef97b48c1526a2c51
Victor Chung <victochung998@yahoo.com>
2019-11-11 14:28:05 -0800

    recreating database for users

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,7 @@ USE classicPart; */
 --   PRIMARY KEY (id)
 -- );
 
-/* CREATE DATABASE userId;
+CREATE DATABASE userId;
 USE userId;
 
 CREATE TABLE Users (
@@ -29,4 +29,4 @@ CREATE TABLE Users (
   Email VARCHAR(100) NOT NULL,
   password VARCHAR(150),
   PRIMARY KEY(id)
-); */
+); 
